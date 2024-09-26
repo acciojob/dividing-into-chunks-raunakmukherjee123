8c99d5a9e757e24176759c961408bc2b56adb16b
Raunak Mukherjee <153613701+raunakmukherjee123@users.noreply.github.com>
2024-09-26 13:04:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,26 +5,23 @@ https://course.acciojob.com/open-web-editor?question=c2fe534a-e405-494a-afd4-56c
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 const divide = (arr, n) => {
   let i=0;
-	let s=0;
-	let arr1=[];
-	let curr=[];
-	while(i<arr.length)
-		{
-			s+=arr[i];
-			if(s>n)
-			{
-				s=arr[i];
-				arr1.push(curr);
-				curr=[];
-			}
-			curr.push(arr[i]);
-			i++;
-		}
-	if(curr.length>0)
-	{
-		arr1.push(curr);
-	}
-	return arr1;
+  let s=0;
+  let arr1=[];
+  let curr=[];
+  while(i<arr.length) {
+    s += arr[i];
+    if(s > n) {
+      s = arr[i]; // Reset sum to current array element
+      arr1.push(curr);
+      curr = [];
+    }
+    curr.push(arr[i]); // Push array element, not sum
+    i++;
+  }
+  if(curr.length > 0) {
+    arr1.push(curr);
+  }
+  return arr1;
 };
 
 const n = prompt("Enter n: ");
