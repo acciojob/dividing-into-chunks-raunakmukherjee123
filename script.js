6ba317b04be260cfc5d16fb4231c82b2ab9dd242
Raunak Mukherjee <153613701+raunakmukherjee123@users.noreply.github.com>
2024-09-26 12:54:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  // Write your code here
+  let i=0;
+	let s=0;
+	const arr=[];
+	const curr=[];
+	while(i<arr.length)
+		{
+			s+=arr[i];
+			if(s>n)
+			{
+				s=0;
+				arr.push(curr);
+				curr=[];
+			}
+			curr.push(s);
+			i++;
+		}
+	if(curr.length>0)
+	{
+		arr.push(curr);
+	}
+	return arr;
 };
 
-const n = prompt("Enter n: ");
+//const n = prompt("Enter n: ");
 alert(JSON.stringify(divide(arr, n)));
